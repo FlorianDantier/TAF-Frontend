ccf9e748fad698b47682fd9e86b92afdcef9168d
Florian <florian.dantier@contentside.com>
2023-02-28 10:00:15 +0100

    commentaire

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,6 +5,7 @@ import App from '../App'
 import { HomePage } from '../pages/HomePage'
 import { NotFoundPage } from '../pages/NotFoundPage'
 
+// Toutes les routes doivent être déclaré avant celle dirigeant vers <NotFoundPage />
 const routes: RouteObject[] = [
   {
     path: '',
